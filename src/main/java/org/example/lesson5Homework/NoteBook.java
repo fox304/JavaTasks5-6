1038b2dfebfbf1a58c990e11d07c28cdf65fb6f8
Maks <nordmax69@gmail.com>
2022-12-29 20:16:29 +0700

    added dependences and notebooks

--- src/main/java/org/example/lesson5Homework/NoteBook.java
+++ src/main/java/org/example/lesson5Homework/NoteBook.java
@@ -1,8 +1,32 @@
 package org.example.lesson5Homework;
 
-public class NoteBook {
+import lombok.Data;
 
-    public NoteBook() {
+@Data
+
+public class NoteBook {
+    String nameOS;
+    String color;
+    Integer price;
+    Double size;
+    Integer ram;
+    Integer coreCount;
+    Double weight;
+    Integer hardDriveVolume;
+
+
+    public NoteBook(String brend, String nameOS, String color,
+                    Integer price, Double size, Integer ram,
+                    Integer coreCount, Double weight, Integer hardDriveVolume) {
+        this.nameOS = nameOS;
+        this.color = color;
+        this.price = price;
+        this.size = size;
+        this.ram = ram;
+        this.coreCount = coreCount;
+        this.weight = weight;
+        this.hardDriveVolume = hardDriveVolume;
     }
+
 }
 
